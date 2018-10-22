45ce0fc1d8a984f05749bbaaedf86361819c6980
prathm92 <1prathm@gmail.com>
2018-10-22 22:35:37 +0530

    CR:Prathm: report generation from nmap output

---
+++ nmaptool-op.py
@@ -0,0 +1,95 @@
+from libnmap.process import NmapProcess
+from libnmap.parser import NmapParser, NmapParserException
+import sys, os
+import re
+from docx import Document
+from docx.shared import Inches
+from time import sleep
+
+output_name = "output"
+
+# start a new nmap scan on localhost with some specific options
+def do_scan(targets, options):
+    parsed = None
+    nmproc = NmapProcess(targets, options)
+    #rc = nmproc.run()
+    rc = nmproc.run_background()
+    while nmproc.is_running():
+        print("Nmap Scan running: ETC: {0} DONE: {1}%".format(nmproc.etc,nmproc.progress))
+        sleep(2)
+    #print(type(nmproc.stdout))
+    file = open(output_name+'.xml','w')
+    file.write(nmproc.stdout)
+    file.close()
+    try:
+        parsed = NmapParser.parse(nmproc.stdout)
+    except NmapParserException as e:
+        print("Exception raised while parsing scan: {0}".format(e.msg))
+
+    return parsed
+
+
+# print scan results from a nmap report
+def print_scan(nmap_report):
+    print("Starting Nmap {0} ( http://nmap.org ) at {1}".format( nmap_report.version, nmap_report.started ))
+    document = Document()
+    document.add_heading('Nmap Scan', 0)	
+
+    no_of_hosts = len(nmap_report.hosts)
+
+    table = document.add_table(rows=1, cols=3)
+    table.style = 'TableGrid'
+    hdr_cells = table.rows[0].cells
+    hdr_cells[0].text = 'ips'
+    hdr_cells[1].text = 'Ports'
+    hdr_cells[2].text = 'Services'
+
+    for host in nmap_report.hosts:
+        row_cells = table.add_row().cells
+        #ip
+        row_cells[0].text = host.address
+        string_port = ''
+        string_service = ''
+        for serv in host.services:
+            #print(serv.state)
+            #print(str(serv.state == "open"))
+            if serv.state == "open":
+                string_port += str(serv.port) + "\n"
+                string_service += serv.service + "\n"
+        string_port = string_port.strip()
+        string_service = string_service.strip()
+        #Ports
+        row_cells[1].text = string_port
+        #Services
+        row_cells[2].text = string_service   
+    document.add_page_break()
+    document.save(output_name+'.docx')
+    print(nmap_report.summary)
+
+
+if __name__ == "__main__":
+    # Get Path from Cmd line
+    pathname = os.path.abspath(sys.argv[0])
+    if len(sys.argv) > 1 :
+        pathname = os.path.abspath(sys.argv[1])
+    print(pathname)
+
+    if len(sys.argv)> 2:
+        output_name =  sys.argv[2]
+
+    all_ips_file = open(pathname,'r')
+
+    all_ips = all_ips_file.read()
+
+    ip_finder = re.compile(''' 
+    	(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s*
+    ''',re.X)
+
+    ips = ip_finder.findall(all_ips)
+    #report = do_scan(ips, "-Pn -T4 -v")
+    
+    #report = do_scan("127.0.0.1", "-sV")
+    if all_ips :
+        print_scan(NmapParser.parse(all_ips))
+    else:
+        print("No results returned")
